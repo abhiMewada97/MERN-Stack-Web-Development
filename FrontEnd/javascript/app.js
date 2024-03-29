7792c7410f227cbb5420f7b1b279a0ddbf03ef82
Abhishek Mewada <abhishek.engineer3@gmail.com>
2024-03-29 08:00:30 +0530

    changes in file app & app6

--- FrontEnd/javascript/app.js
+++ FrontEnd/javascript/app.js
@@ -1,35 +1,39 @@
-console.log("Abhishek Mewada");
-console.log("Hello everyone");
-let pencilPrice = 10;
-let erasorPrice = 5;
 
-let total = "Total price is "+ pencilPrice + erasorPrice + " Rupees";
-console.log(total);
 
-total = "Total price is "+ (pencilPrice + erasorPrice) + " Rupees";
-console.log(total);
 
-// ( ` `) back ticks 
-total = `Total price is ${pencilPrice + erasorPrice} Rupees`;
-console.log(total);
 
-console.log(`Total price is ${pencilPrice + erasorPrice} Rupees`);
+// console.log("Abhishek Mewada");
+// console.log("Hello everyone");
+// let pencilPrice = 10;
+// let erasorPrice = 5;
 
-console.log("Total price is", pencilPrice + erasorPrice , " Rupees");
+// let total = "Total price is "+ pencilPrice + erasorPrice + " Rupees";
+// console.log(total);
 
-let str = "good string";
-if((str[0]>='a' || str[0] <= 'z') && str.length > 3) {
-    console.log(str);
-}
-else {
-    console.log(str);
-}
+// total = "Total price is "+ (pencilPrice + erasorPrice) + " Rupees";
+// console.log(total);
+
+// // ( ` `) back ticks 
+// total = `Total price is ${pencilPrice + erasorPrice} Rupees`;
+// console.log(total);
+
+// console.log(`Total price is ${pencilPrice + erasorPrice} Rupees`);
+
+// console.log("Total price is", pencilPrice + erasorPrice , " Rupees");
+
+// let str = "good string";
+// if((str[0]>='a' || str[0] <= 'z') && str.length > 3) {
+//     console.log(str);
+// }
+// else {
+//     console.log(str);
+// }
 
 // alert("Something is wrong");
 // console.log("simple log");
 // console.error("simple error");
 // console.warn("simple warning");
 
-let fName = prompt("Enter First name");
-let sName = prompt("Enter Second name");
-alert("Welcome "+ fName +" "+ sName);
+// let fName = prompt("Enter First name");
+// let sName = prompt("Enter Second name");
+// alert("Welcome "+ fName +" "+ sName);
--- FrontEnd/javascript/app6.js
+++ FrontEnd/javascript/app6.js
@@ -4,26 +4,87 @@
 // call function by variable name
 // esh tarah se function ko define karne ko hi function expressions kahte hai
 
-let sum = function(a,b) {
+// let sum = function(a,b) {
 
-    return a+b;
-}
+//     return a+b;
+// }
 
-console.log(sum);
-console.log(sum(1,5));
+// console.log(sum);
+// console.log(sum(1,5));
 
-// Higher order function...................
+// // Higher order function...................
 
-function multipleGreet(func, n) {
+// function multipleGreet(func, n) {
 
-    for(let i=1; i<=n; i++) {
-        func();
+//     for(let i=1; i<=n; i++) {
+//         func();
+//     }
+// }
+
+// let greet = function() {
+//     console.log("hello");
+// }
+
+// multipleGreet(greet,2);
+// multipleGreet(function() {console.log("namaste");},3);
+
+
+
+
+// higher order function
+// function oddOrEvenFactory(request) {
+//     if(request=="odd") {
+//         let odd = function(n) {
+//             console.log(n%2==1);
+//         }
+//         return odd;
+//         // return function(n) {
+//         //     console.log(n%2==1);
+//         // }
+//     }
+//     else if(request=="even") {
+//         return function(n) {
+//             console.log(n%2==0);
+//         }
+//     }
+//     else {
+//         console.log("wrong request");
+//     }
+// }
+
+// let request= "odd";
+
+
+// Methods
+// Action that can be performed on an object
+// const calculator = {
+//     add: function(a,b) {
+//         return a+b;
+//     },
+//     sub: function(a,b) {
+//         return a-b;
+//     },
+//     mul: function(a,b) {
+//         return a*b;
+//     }
+// };
+
+// let sum = calculator.add(1,4);
+// console.log(sum);
+
+
+// short hand
+const calculator = {
+    add(a,b) {
+        return a+b;
+    },
+    sub(a,b) {
+        return a-b;
+    },
+    mul(a,b) {
+        return a*b;
     }
-}
+};
 
-let greet = function() {
-    console.log("hello");
-}
-
-multipleGreet(greet,2);
-multipleGreet(function() {console.log("namaste");},3);
+let sum = calculator.add(1,4);
+console.log(sum);
